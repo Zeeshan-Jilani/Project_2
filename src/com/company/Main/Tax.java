946edb12711f6b2165d4fd58d9e033f8ee982eee
Hello Vistula 2 <zjilani1@stu.vistula.edu.pl>
2022-03-08 16:22:30 +0100

    Task_5_Correction

--- src/com/company/Main/Tax.java
+++ src/com/company/Main/Tax.java
@@ -1,13 +1,18 @@
 package com.company.Main;
 
+import java.math.BigDecimal;
+
 public class Tax {
     public static void main(String[] args){
 
-        double net_cost = 9.99;
-        float Gross_value = (float) (9.99 + 0.23);
+        BigDecimal net_cost, Gross_value,products_sold;
+        net_cost = new BigDecimal("9.99");
+        Gross_value = BigDecimal.valueOf(9.99 + 0.23);
+        products_sold = new BigDecimal(10000);
+        //float Gross_value = (float) (9.99 + 0.23);
 
         System.out.println("The net cost of product: " + net_cost);
         System.out.println("The gross value of product: " + Gross_value);
-        System.out.println("\nAmount after selling 10000 products: " + net_cost * 10000);
+        System.out.println("\nAmount after selling 10000 products: " + Gross_value.multiply(products_sold));
     }
 }
